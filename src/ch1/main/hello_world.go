7d5a052de485e2ee33ccf709e0af3cfd77f49bb5
hibernate-pano <362472407@qq.com>
2022-06-10 23:09:06 +0800

    Initial commit

---
+++ src/ch1/main/hello_world.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+//功能实现
+func main() {
+	fmt.Println(os.Args)
+	if len(os.Args) > 1 {
+		fmt.Println("Hello World", os.Args[1])
+	}
+	fmt.Println("Hello World")
+	os.Exit(0)
+}
