d86b05894dbd140138cd5c2d42b97ec555e2f657
hibernate-pano <362472407@qq.com>
2022-06-11 20:28:03 +0800

    init

--- src/ch3/type_test/type_test.go
+++ src/ch3/type_test/type_test.go
@@ -1,6 +1,9 @@
 package type_test
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 type MyInt int64
 
@@ -12,4 +15,28 @@ func TestImplicit(t *testing.T) {
 	var c MyInt
 	c = MyInt(b)
 	t.Log(a, b, c)
+}
+
+func TestMath(t *testing.T) {
+	a := math.MaxInt64
+	b := math.MaxFloat64
+	c := math.MaxUint32
+	t.Log(a, b, c)
+}
+
+// go 不支持指针运算
+func TestPoint(t *testing.T) {
+	a := 1
+	aPtr := &a
+	t.Log(a, aPtr)
+	t.Logf("%T %T", a, aPtr)
+}
+
+func TestString(t *testing.T) {
+	var s string
+	t.Log("*" + s + "*˚")
+	t.Log(len(s))
+	if s == "" {
+		t.Log("It`s Blank")
+	}
 }
