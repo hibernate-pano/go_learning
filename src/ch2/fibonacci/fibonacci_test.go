c409241e7697a659ad7579047b446e39fe9b818a
hibernate-pano <362472407@qq.com>
2022-06-11 17:25:51 +0800

    init

--- src/ch2/fibonacci/fibonacci_test.go
+++ src/ch2/fibonacci/fibonacci_test.go
@@ -1,16 +1,23 @@
 package fibonacci
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestFibonacciList(t *testing.T) {
 	a, b := 1, 1
-	fmt.Print(a + b)
+	t.Log(a)
 	for i := 0; i < 5; i++ {
+		t.Log(b)
 		tmp := a
 		a = b
 		b = tmp + a
 	}
+}
+
+// go 中可以不通过临时变量来进行数值交换
+func TestExchangeValue(t *testing.T) {
+	a, b := 1, 2
+	a, b = b, a
+	t.Log(a, b)
 }
