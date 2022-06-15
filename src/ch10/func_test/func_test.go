a6859715b43807958b22fefb74bedd96e6269afd
hibernate-pano <362472407@qq.com>
2022-06-15 13:32:21 +0800

    init

--- src/ch10/func_test/func_test.go
+++ src/ch10/func_test/func_test.go
@@ -36,3 +36,29 @@ func TestFuncInFunc(t *testing.T) {
 	timeFunc := timeSpent(slowFunc)
 	timeFunc(10)
 }
+
+// 测试可变长度参数
+func TestVarParam(t *testing.T) {
+	t.Log(SumInt(1, 2, 3))
+}
+
+// 可变长度参数方法
+func SumInt(params ...int) int {
+	sum := 0
+	for _, p := range params {
+		sum += p
+	}
+	return sum
+}
+
+// 延迟执行defer
+func TestDeferFunc(t *testing.T) {
+	// defer 修饰的方法在方法返回之前或者抛出异常之前执行
+	// 类似 java 的 try..catch..finally...结构中的finally，必定会执行的方法
+	defer func() {
+		t.Log("defer func executed!!")
+	}()
+	t.Log("begin")
+	// 类似手动抛出异常信息，panic之后的方法不再执行
+	panic("Fatal Err")
+}
