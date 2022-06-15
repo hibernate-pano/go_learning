c81559e7f8d441859e82bdb7124b924cca2e5829
hibernate-pano <362472407@qq.com>
2022-06-15 09:05:13 +0800

    init

---
+++ src/ch9/string_test/string_test.go
@@ -0,0 +1,30 @@
+package string_test
+
+import (
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestStringFunc(t *testing.T) {
+	s := "a,b,c"
+	parts := strings.Split(s, ",")
+	t.Log(parts)
+
+	for _, part := range parts {
+		t.Log(part)
+	}
+
+	ss := strings.Join(parts, "-")
+	t.Log(ss)
+}
+
+func TestStringConv(t *testing.T) {
+	// 数字转字符串
+	s := strconv.Itoa(10)
+	t.Log("string" + s)
+	// 字符串转数字,需要显示处理转换错误逻辑
+	if i, err := strconv.Atoi("20"); err == nil {
+		t.Log(10 + i)
+	}
+}
